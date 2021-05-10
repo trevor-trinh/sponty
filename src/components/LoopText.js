0e21730a119dac9f3f9860b285024db66f4b820f
Trevor Trinh <50549133+trevor-trinh@users.noreply.github.com>
2021-05-09 21:29:56 -0700

    broken two scroll, gonna make it into a component to pass props but saving just in case

--- src/components/LoopText.js
+++ src/components/LoopText.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect, useRef } from "react";
 import TextLoop from "react-text-loop";
 
-
 function LoopText(props) {
-  const [options, setOptions] = useState(["Go", "Eat", "Read"]);
-  const [rollTime, setRollTime] = useState(0);
+  let actions = ["Go", "Eat", "Read"]
+  let actionSpecPair = {
+    "Go": ["Park", "Mall", "Beach with friends oh wait you don'thave friends"],
+    "Eat": ["Lamb", "Noodles", "Rice"],
+    "Read": ["三国", "Boys in the Boat", "One"],
+  };
+
+  const [actionOptions, setActionOptions] = useState(actions);
+  const [specOptions, setSpecOptions] = useState(null);
+
+  const [actionRollTime, setActionRollTime] = useState(0);
+  const [specRollTime, setSpecRollTime] = useState(0);
+
+
+  const [finalAction, setFinalAction] = useState(null);
+  const [finalSpec, setFinalSpec] = useState(null);
+
+  const actionChange = ({ currentEl, currentWordIndex }) => {
+    setFinalAction(currentEl);
+    console.log(finalAction);
+  };
+
+  const specChange = ({ currentEl, currentWordIndex }) => {
+    setFinalSpec(currentEl);
+    console.log(finalSpec);
+  };
 
   // can use this to update list every few days from the google sheet
   // useEffect(() => {
@@ -25,8 +48,8 @@ function LoopText(props) {
 
   useEffect(() => {
     const intervalStartTimeout = setTimeout(() => {
-      console.log("start");
-      setRollTime(100);
+      console.log("🏁 start action roll");
+      setActionRollTime(100);
     }, 1000);
     return () => {
       clearTimeout(intervalStartTimeout);
@@ -35,18 +58,52 @@ function LoopText(props) {
 
   useEffect(() => {
     const intervalStopTimeout = setTimeout(() => {
-      setRollTime(0);
-      console.log("stop");
+      setActionRollTime(0);
+      console.log("🛑 stop action roll");
+      
+      setSpecOptions(actionSpecPair[finalAction])
     }, 3000);
-
     return () => {
       clearTimeout(intervalStopTimeout);
     };
   }, [props.reroll]);
 
+  // I repeat the same code not sure how i can fix this
+  // maybe custom hooks?
+
+  useEffect(() => {
+    const intervalStartTimeout = setTimeout(() => {
+      console.log("start spec roll 🏁");
+      setSpecRollTime(100);
+    }, 4000);
+    return () => {
+      clearTimeout(intervalStartTimeout);
+    };
+  }, [props.reroll]);
+
+  useEffect(() => {
+    const intervalStopTimeout = setTimeout(() => {
+      setSpecRollTime(0);
+      console.log("stop spec roll 🛑");
+    }, 6000);
+    return () => {
+      clearTimeout(intervalStopTimeout);
+    };
+  }, [props.reroll]);
+  
+
   return (
     <>
-      <TextLoop interval={rollTime} children={options} onChange={(e) => console.log(e)} /> random stuff
+      <TextLoop
+        interval={actionRollTime}
+        children={actionOptions}
+        onChange={actionChange}
+      />{' '}
+      <TextLoop
+        interval={specRollTime}
+        children={specOptions}
+        onChange={specChange}
+      />
     </>
   );
 }
